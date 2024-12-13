72e9f33e82e8361ed0318167aa1b3c70bfd391cc
Alexandra0707 <alex07marta@gmail.com>
2024-12-13 13:35:03 +0000

    update

---
+++ src/components/App/App.jsx
@@ -0,0 +1,25 @@
+import Profile from "../Profile/Profile";
+import "modern-normalize";
+import userData from "../../assets/userData.json";
+import FriendList from "../FriendList/FriendList";
+import friends from "../../assets/friends.json";
+import TransactionHistory from "../TransactionHistory/TransactionHistory";
+import transactions from "../../assets/transactionHistory.json";
+
+const App = () => {
+  return (
+    <div>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+};
+
+export default App;
